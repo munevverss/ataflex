8b503a0072afd6129a9c499f4392f89f1835a43a
Münevvers <ademunevver@hotmail.com>
2018-02-21 23:36:28 +0300

    First Commit

---
+++ Ataflex/Ataflex/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace Ataflex.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Default/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult VazgecilmezYanHaklariniz()
+        {
+            return View();
+        }
+        public ActionResult Help()
+        {
+            return View();
+        }
+        public ActionResult SSS()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+	}
+}
---
+++ Ataflex/Ataflex/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ataflex.MvcApplication" Language="C#" %>
